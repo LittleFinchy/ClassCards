968731af77f879f091f1a9ed62638cf786da81f7
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-10-12 14:22:37 -0400

    beta layout
    nothing final
    
    side drawer worked pretty well
    
    keep learning basics!

---
+++ lib/drawer.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+Widget drawer = Drawer(
+  child: ListView(
+    padding: EdgeInsets.zero,
+    children: const <Widget>[
+      DrawerHeader(
+        decoration: BoxDecoration(
+          color: Colors.pink,
+        ),
+        child: Text('Drawer Header',
+            style: TextStyle(color: Colors.white, fontSize: 24)),
+      ),
+      ListTile(
+        leading: Icon(Icons.message),
+        title: Text('Messages'),
+      ),
+      ListTile(
+        leading: Icon(Icons.account_circle),
+        title: Text('Profile'),
+      ),
+      ListTile(
+        leading: Icon(Icons.settings),
+        title: Text('Settings'),
+      ),
+    ],
+  ),
+);
---
+++ lib/settings.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color bodyColor = Colors.indigo[100];
+Color barColor = Colors.indigo[400];
